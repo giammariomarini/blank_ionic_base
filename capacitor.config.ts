45d24ae382024e961ab42a65dd7ea396ab7c2563
Giammario <giammariomarini@gmail.com>
2025-02-07 21:15:06 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prova_err',
+  webDir: 'www'
+};
+
+export default config;
